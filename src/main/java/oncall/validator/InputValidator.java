7115553d03ba287bfed83634d8e05b0987d3727b
jyt6640 <jyt6640@gmail.com>
2025-12-31 21:52:45 +0900

    feat: 월, 요일 입력값 검증 로직 수정
    - 월 1~12월인지 검증 로직 구현
    - 요일이 월~일요일 사이에 있는지 검증 로직 구현

--- src/main/java/oncall/validator/InputValidator.java
+++ src/main/java/oncall/validator/InputValidator.java
@@ -1,9 +1,13 @@
 package oncall.validator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputValidator {
 
     private static final String MONTH_AND_DAY_REGEX = "^[0-9]+,[ㄱ-ㅎ가-힣]+$";
     private static final String WORKER_REGEX = "^[ㄱ-ㅎ가-힣A-Za-z]+(,[ㄱ-ㅎ가-힣A-Za-z]+)*$";
+    private static final List<String> VALID_DAYS = Arrays.asList("일", "월", "화", "수", "목", "금", "토");
 
 
     public void validateMonthAndDay(String input) {
@@ -29,6 +33,15 @@ public class InputValidator {
 
     private void monthAndDayInput(String input) {
         if (!input.matches(MONTH_AND_DAY_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
+        String[] parts = input.split(",");
+        int month = Integer.parseInt(parts[0]);
+        String day = parts[1];
+        if (month < 1 || month > 12) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
+        if (!VALID_DAYS.contains(day)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
         }
     }
