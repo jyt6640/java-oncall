c110004f65ef887ea3c345f72a7d03d355a00020
jyt6640 <jyt6640@gmail.com>
2026-01-02 23:23:04 +0900

    feat: WorkDay 도메인 생성
    - 월, 일, 요일로 객체 생성 기능 구현
    - 법적 공휴일인지 확인하는 기능 구현
    - 휴일인지 확인하는 기능 구현

---
+++ src/main/java/oncall/domain/WorkDay.java
@@ -0,0 +1,49 @@
+package oncall.domain;
+
+public class WorkDay {
+
+    private final int month;
+    private final int date;
+    private final Day day;
+    private final boolean isLegalHoliday;
+    private final boolean isHoliday;
+
+    public WorkDay(int month, int date, Day day) {
+        this.month = month;
+        this.date = date;
+        this.day = day;
+        this.isLegalHoliday = isLegalHolidayY(month, date);
+        this.isHoliday = isLegalHoliday || day == Day.SATURDAY || day == Day.SUNDAY;
+    }
+
+    private boolean isLegalHolidayY(int month, int date) {
+        return (month == 1 && date == 1) ||
+                (month == 3 && date == 1) ||
+                (month == 5 && date == 5) ||
+                (month == 6 && date == 6) ||
+                (month == 8 && date == 15) ||
+                (month == 10 && date == 3) ||
+                (month == 10 && date == 9) ||
+                (month == 12 && date == 25);
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public Day getDay() {
+        return day;
+    }
+
+    public boolean isLegalHoliday() {
+        return isLegalHoliday;
+    }
+
+    public boolean isHoliday() {
+        return isHoliday;
+    }
+}
