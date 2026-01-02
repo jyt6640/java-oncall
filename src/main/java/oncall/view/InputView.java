dc13ac845b50fb7863d5908957a373531f637379
jyt6640 <jyt6640@gmail.com>
2026-01-02 23:00:37 +0900

    fix: 입력 형식에 맞도록 변경

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -4,23 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String readMonthAndDay() {
-        System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요>");
+        System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
         String value = readLine();
-        System.out.println();
         return value;
     }
 
     public String readWeekdayWorker() {
-        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
         String value = readLine();
-        System.out.println();
         return value;
     }
 
     public String readHolidayWorker() {
-        System.out.print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+        System.out.print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
         String value = readLine();
-        System.out.println();
         return value;
     }
 
